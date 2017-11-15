35b0088020bf4e66e3b3f0afafbdf48ed113a507
Marcin Grzywaczewski <killavus@gmail.com>
2017-11-16 00:48:19 +0100

    Added main loop structure; Removed unused use in errors mod

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,26 @@ extern crate error_chain;
 mod errors;
 
 use errors::*;
+use discord::Discord;
+use discord::State;
 
-pub fn run(_bot_key: &str) -> Result<()> {
-    Ok(())
+pub fn run(bot_key: &str) -> Result<()> {
+    let discord =
+        Discord::from_bot_token(bot_key).chain_err(|| "Failed to initialize discord client.")?;
+
+    let (mut connection, ready_state) = discord
+        .connect()
+        .chain_err(|| "Failed to initialize connection.")?;
+
+    let mut state = State::new(ready_state);
+
+    loop {
+        if let Ok(event) = connection.recv_event() {
+            state.update(&event);
+        } else {
+            eprintln!("failed to receive an event.");
+        }
+    }
 }
 
 #[cfg(test)]
