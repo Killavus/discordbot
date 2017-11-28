89327bee90398ee79fb301a0ac6fd98f2c0828dc
Marcin Grzywaczewski <killavus@gmail.com>
2017-11-28 03:58:40 +0100

    Fixed a bug with cleaning channel process crashing with only 1 message to delete.

--- src/discord_utils.rs
+++ src/discord_utils.rs
@@ -22,9 +22,15 @@ pub fn clear_channel_messages<T: AsRef<Discord>>(discord: T, channel_id: Channel
   for messages_chunk in messages.chunks(100) {
     let message_ids: Vec<MessageId> = messages_chunk.iter().map(|message| message.id).collect();
 
-    discord_client
-      .delete_messages(channel_id, &message_ids)
-      .chain_err(|| "Failed to delete messages.")?;
+    if message_ids.len() > 1 {
+      discord_client
+        .delete_messages(channel_id, &message_ids)
+        .chain_err(|| "Failed to delete messages.")?;
+    } else {
+      discord_client
+        .delete_message(channel_id, *message_ids.first().unwrap())
+        .chain_err(|| "Failed to delete a message.")?;
+    }
   }
 
   Ok(())
